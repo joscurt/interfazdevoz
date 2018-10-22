cb3276b275af09f614b89c72635c1e82dcab7ea1
Jose <joscurt@gmail.com>
2018-10-22 03:44:31 -0300

    Agregue metodos a la API

--- src/odontologia/odontologia_api/views.py
+++ src/odontologia/odontologia_api/views.py
@@ -33,3 +33,16 @@ class HelloApiView(APIView):
             return Response({'message': message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Updatear el objeto"""
+
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+        """Solo actualiza los campos provistos en la solicitud"""
+
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method':'delete'})
